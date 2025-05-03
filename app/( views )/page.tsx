9412712110372ec24e6e3ad0932515b48650d828
YasithaRenuk <126389901+YasithaRenuk@users.noreply.github.com>
2025-05-03 13:52:01 +0530

    feat: add skeleton card

--- app/( views )/page.tsx
+++ app/( views )/page.tsx
@@ -1,12 +1,10 @@
 "use client";
-
-import { useEffect, useState } from "react";
 import Hero from "../components/Home/Hero";
 import ListingSection from "../components/Home/ListingSection";
 import { SearchParams } from "../components/Home/SearchBar";
-import { set } from "mongoose";
 import { useQuery } from "@tanstack/react-query";
 import { getNearPropertiesByLongitudeLatitude } from "@/lib/queryOptions";
+import ListingSectionSkeleton from "../components/Home/ListingSectionSkeleton";
 
 
 export default function HomePage() {
@@ -15,33 +13,24 @@ export default function HomePage() {
     console.log("Search params:", { type, range, area });
   }
 
-  const { isPending, isError, data, error } = useQuery(
+  const {isPending,isError, data, error } = useQuery(
     getNearPropertiesByLongitudeLatitude("79", "6", "1")
   );
 
-  if(isPending){
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <h1 className="text-2xl font-bold">Loading...</h1>
-      </div>
-    );
-  }
-
-  if(isError){
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <h1 className="text-2xl font-bold">Error</h1>
-      </div>
-    );
-  }
-
+  
   return (
     <div>
       <div className="h-auto md:h-screen">
         <Hero onSearch={handleSearch} />
       </div>
       <div>
-        <ListingSection listings={data?.properties || []} />
+        {isPending ? (
+          <ListingSectionSkeleton/>
+        ) : isError ? (
+          <h1 className="text-2xl font-bold">Error</h1>
+        ) : (
+          <ListingSection listings={data?.properties || []} />
+        )}
       </div>
     </div>
   );
